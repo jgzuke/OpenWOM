3013d5459f4fe2db07bbc49843a7b1224ed38d39
jgzuke <jgzuke@gmail.com>
2014-12-17 00:02:55 -0500

    the other images

--- gen/com/spritelib/R.java
+++ gen/com/spritelib/R.java
@@ -12,10 +12,10 @@ public final class R {
 		public static final int activity_vertical_margin = 0x7f060001;
 	}
 	public static final class drawable {
-		public static final int ic_launcher = 0x7f0200ff;
+		public static final int ic_launcher = 0x7f020186;
 	}
 	public static final class id {
-		public static final int action_settings = 0x7f0c0064;
+		public static final int action_settings = 0x7f0c004e;
 	}
 	public static final class layout {
 		public static final int activity_main = 0x7f03001f;
